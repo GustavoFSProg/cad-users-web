03868e3d1cefb359736a3e60796f0570ae2fb5eb
Gustavo <gustavo.prog40@gmail.com>
2022-06-27 18:21:12 -0300

    dassh

--- src/pages/Dashboard/style-app.js
+++ src/pages/Dashboard/style-app.js
@@ -1,14 +1,18 @@
 import styled from 'styled-components'
+import Dash from '../../assets/dash-3.jpg'
 
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  width: 100vw;
+  width: 98vw;
   height: 100vh;
+  background: url(${Dash}) no-repeat;
+  background-size: cover;
 
   @media screen and (max-width: 600px) {
     margin-top: 40px;
+    width: 100vw;
   }
 `
 
@@ -24,7 +28,7 @@ export const Image = styled.img`
 export const Title = styled.span`
   font-family: Titan One;
   font-size: 2.5rem;
-  color: #007399;
+  color: #00aaff;
 
   display: flex;
   justify-content: start;
